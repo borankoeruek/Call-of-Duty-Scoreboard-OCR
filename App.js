5d22c6842b445978fb1e3ff2c915dbb09fc572c3
vendom2k <borankrk@gmail.com>
2022-02-16 16:38:55 +0100

    working prototype

---
+++ App.js
@@ -0,0 +1,29 @@
+import { createWorker, PSM } from "tesseract.js";
+import process, { exit } from "process";
+import { readFile } from "fs/promises";
+import startExtractingFlow from "./startExtractingFlow.js";
+
+const config = JSON.parse(
+  await readFile(new URL("./helpers/config.json", import.meta.url))
+);
+
+(async () => {
+  const URL = process.argv.slice(2)[0];
+  if (!URL) {
+    console.log("Please provide an image URL as a argument");
+    exit(1);
+  }
+
+  const worker = createWorker();
+
+  await worker.load();
+  await worker.loadLanguage("eng");
+  await worker.initialize("eng");
+  await worker.setParameters({
+    tessedit_pageseg_mode: PSM.SINGLE_BLOCK,
+  });
+
+  const result = await startExtractingFlow(worker, URL, config);
+
+  console.log("results", result);
+})();
---
+++ startExtractingFlow.js
@@ -0,0 +1,56 @@
+const startExtractingFlow = async (worker, URL, config) => {
+  const results = {};
+
+  for (const settingName in config.coldwarScoreboard.cropSettings) {
+    console.log("scanning", settingName);
+
+    const currentSettings = config.coldwarScoreboard.cropSettings[settingName];
+
+    if (Array.isArray(currentSettings)) {
+      // loop trough the array
+
+      for (const index in currentSettings) {
+        const result = await worker.recognize(URL, {
+          rectangle: currentSettings[index],
+        });
+        const extracedText = result.data.text.replace("\n", "");
+
+        results[settingName + index] = extracedText;
+      }
+    } else {
+      const result = await worker.recognize(URL, {
+        rectangle: currentSettings,
+      });
+
+      const extracedText = result.data.text.replace("\n", "");
+
+      if (settingName.slice(-5) === "Score") {
+        results[settingName] = extractNumbersFromString(extracedText);
+
+        continue;
+      }
+
+      results[settingName] = extracedText;
+    }
+  }
+
+  await worker.terminate();
+
+  return results;
+};
+
+const extractNumbersFromString = (string) => {
+  const array = string.split("");
+  let number = "";
+
+  for (let i = 0; i < array.length; i++) {
+    const char = parseInt(array[i]);
+    if (!isNaN(char)) {
+      number += char;
+    }
+  }
+
+  return parseInt(number);
+};
+
+export default startExtractingFlow;
